435b884ed0619b2670bdca3f2a6bf5f4c51fbb97
JesseLivingston1 <jesselivingston1@gmail.com>
2014-07-10 01:02:17 -0400

    beginning site architecture

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+//resize section heights on load//
+
+$(document).ready(function() {
+        var heightwindow = $(window).height();
+        $('#landing').css('height', heightwindow +'px');
+    });
+
