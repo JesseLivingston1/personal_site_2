ebe76436644aa5b288a29c3b7563abe3472b527d
JesseLivingston1 <jesselivingston1@gmail.com>
2014-07-18 02:12:18 -0400

    social media links and launched to subdomain

--- js/main.js
+++ js/main.js
@@ -14,3 +14,45 @@ $(document).ready(function() {
 $(window).load(function() {
 	 $('#loading').fadeOut(1000);
 });
+
+//chart description fade//
+$(document).ready(function () {
+    var i = $(window).width();
+    if (i < 820) {
+        $("#description .step").each(function () {
+            $(this).css({
+                opacity: 0,
+                visibility: "visible"
+            });
+        });
+    }
+    if (i < 820) {
+        $(".one").animate({
+            opacity: 1
+        });
+    }
+    setTimeout(function () {
+        if (i < 820) {
+            $(".two").animate({
+                opacity: 1
+            });
+        }}, 200);
+    setTimeout(function () {
+        if (i < 820) {
+            $(".three").animate({
+                opacity: 1
+            });
+        }}, 400);
+    setTimeout(function () {
+        if (i < 820) {
+            $(".four").animate({
+                opacity: 1
+            });
+        }}, 600);
+    setTimeout(function () {
+        if (i < 820) {
+            $(".five").animate({
+                opacity: 1
+            });
+        }}, 800);
+});
